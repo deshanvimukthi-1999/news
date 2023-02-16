0d474873914e1874523a49e7957228215a03c401
Deshan Vimukthi <vimudeshan123@gmail.com>
2023-02-16 17:52:44 +0530

    Added project files

---
+++ api/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import postRoutes from "./routes/posts.js";
+import cookieParser from "cookie-parser";
+import multer from "multer";
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../webapp/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
+
+app.use("/api/posts", postRoutes);
+
+app.listen(8800, () => {
+  console.log("Connected!");
+});
---
+++ webapp/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <span>
+        Made with <b>React.js</b> <b>Node.js</b> & <b>MySQL database</b>.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
